6ca562f4f76120f938a8fee3338c22d9fd3f0f40
enma <enmaundostres@gmail.com>
2024-02-21 20:29:58 -0300

    main rebasa a autenticacion

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+<<<<<<< HEAD
 import { Text } from 'react-native'
 
 const PerfilPantalla = () => {
   return (
     <Text>PerfilPantalla</Text>
+=======
+
+const PerfilPantalla = () => {
+  return (
+    <>
+    </>
+>>>>>>> 2208319 (renombrando pantallas)
   )
 }
 
