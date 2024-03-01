f370c113c375b5bce212c5ca45d79921e90587e5
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2024-03-01 18:09:00 -0300

    Modificando-Pantalla-Ayuda

--- screens/AyudaScreen.jsx
+++ screens/AyudaScreen.jsx
@@ -1,76 +1,93 @@
-import { Image, ScrollView, StatusBar, StyleSheet, Text, View, } from 'react-native';
+import { Image, Linking, SafeAreaView, ScrollView, Text, View, } from 'react-native';
 import BotonPrimario from '../Componentes/BotonPrimario';
+import AyudaScreenStyles from '../styles/AyudaScreenStyles';
+import { Collapse, CollapseBody, CollapseHeader } from 'accordion-collapse-react-native';
 
 const SoportePantalla = () => {
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.boxTitleContainer}>
-          <Text style={styles.text}>
-            ¿Necesitas ayuda?
-          </Text>
-        </View>
-        <View style={styles.boxTitleContainer}>
-          <Text style={styles.title}>
-            Presiona el boton y te ayudamos a la brevedad
-          </Text>
-        </View>
-        <View>
-          <Image src='./assets/support.png' />
-        </View>
-        <View style={styles.containerBtn}>
-        </View>
-        <View style={styles.boxTitleContainer}>
-          <BotonPrimario text='Ayuda' />
-        </View>
-        <View style={styles.boxTitleContainer}>
-          <Text style={styles.text}>
-            Preguntas Frecuentes
-          </Text>
-        </View>
 
-      </View>
-    </ScrollView>
+  const handleWhatsAppPress = () => {
+    const phoneNumber = '+543794681650'; // Reemplaza con el número de teléfono de WhatsApp al que deseas enviar el mensaje
+
+    // Construye la URL de WhatsApp con el número de teléfono
+    const whatsappUrl = `whatsapp://send?phone=${phoneNumber}&text=${wppMsg}`;
+
+    const wppMsg = 'Tengo un inconveniente con la app';
+
+    // Intenta abrir la URL en la aplicación de WhatsApp
+    Linking.openURL(whatsappUrl)
+      .catch(err => console.error('Error al abrir WhatsApp:', err));
+  };
+
+
+
+  return (
+    <SafeAreaView>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={AyudaScreenStyles.container}>
+          <View style={AyudaScreenStyles.boxTitleContainer}>
+            <Text style={AyudaScreenStyles.title}>
+              ¿Necesitas ayuda?
+            </Text>
+          </View>
+          <View style={AyudaScreenStyles.boxTitleContainer}>
+            <Text style={AyudaScreenStyles.subTitle}>
+              Presiona el boton y te ayudamos a la brevedad
+            </Text>
+          </View>
+          <View style={AyudaScreenStyles.imageContainer}>
+            <Image source={require('../assets/support.png')} />
+          </View>
+          <View style={AyudaScreenStyles.containerBtn}>
+            <BotonPrimario onPress={handleWhatsAppPress} text='Ayuda' />
+          </View>
+          <View style={AyudaScreenStyles.boxTitleContainer}>
+            <Text style={AyudaScreenStyles.subTitle}>
+              Preguntas Frecuentes
+            </Text>
+          </View>
+          <View style={AyudaScreenStyles.boxCollapseContainer}>
+            {/* //Simple collapsable */}
+            <Collapse style={AyudaScreenStyles.collapseContainer}>
+              <CollapseHeader>
+                <View>
+                  <Text style={AyudaScreenStyles.paragraph}>¿En el viaje nos entregan comida?</Text>
+                </View>
+              </CollapseHeader>
+              <CollapseBody>
+                <Text style={AyudaScreenStyles.textCollape}>No, por el momento no entregamos comida abordo</Text>
+              </CollapseBody>
+            </Collapse>
+            {/* //Simple collapsable */}
+            <Collapse style={AyudaScreenStyles.collapseContainer}>
+              <CollapseHeader>
+                <View>
+                  <Text style={AyudaScreenStyles.paragraph}>¿Que pasa si cancelo mi viaje?</Text>
+                </View>
+              </CollapseHeader>
+              <CollapseBody>
+                <Text style={AyudaScreenStyles.textCollape}>El viaje solo puede ser cancelado 24hs despues de haberlo sacado</Text>
+              </CollapseBody>
+            </Collapse>
+            {/* //Simple collapsable */}
+            <Collapse>
+              <CollapseHeader>
+                <View>
+                  <Text style={AyudaScreenStyles.paragraph}>¿Puedo viajar sin mi comprobante?</Text>
+                </View>
+              </CollapseHeader>
+              <CollapseBody>
+                <Text style={AyudaScreenStyles.textCollape}>No es posible viajar sin el comprobante, ya que de esa manera la empresa sabe que podes viajar</Text>
+              </CollapseBody>
+            </Collapse>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+
   );
 };
 
 export default SoportePantalla;
 
-//OPCIONES ----------------------------------------------------
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    borderWidth: 2,
-    borderColor: 'pink',
-    padding: 20,
-  },
-  title: {
-    color: '#F46262',
-    fontSize: 24,
-    fontWeight: '400',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    width: 300,
-    margin: 10,
-  },
-  text: {
-    color: '#1E1E1E',
-    fontSize: 36,
 
-  },
-  btn: {
-    backgroundColor: 'gray',
-  },
-  boxTitleContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  containerBtn: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-})
 
